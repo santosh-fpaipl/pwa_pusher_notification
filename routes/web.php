2401c88afce08d7c81b7179a592239a56587d863
Santosh <santosh9mca@gmail.com>
2023-12-08 17:41:17 +0530

    minishlink/web-push,  npm web-push, laravel/ui packages has been installed.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/refresh', function(Request $request){
     return view('refresh');
 })->name('refresh');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
